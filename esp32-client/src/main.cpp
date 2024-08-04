33653b31076e20a0c8388ddc8c179b76e140c5ab
tomas.lajda <tomas.lajda0@gmail.com>
2024-08-04 21:55:15 +0200

    init default values in db

--- esp32-client/src/main.cpp
+++ esp32-client/src/main.cpp
@@ -10,6 +10,27 @@ FirebaseData firebaseData;
 FirebaseAuth auth;
 FirebaseConfig config;
 
+const int inputPins[] = {16, 17};
+const int outputPins[] = {18, 19};
+const int analogToDigital = 32;
+const int digitalToAnalog = 25;
+
+void initializePinData();
+
+void streamCallback(StreamData data) {
+  Serial.println("Stream data received:");
+  Serial.println(data.streamPath());
+  Serial.println(data.dataPath());
+  Serial.println(data.dataType());
+  Serial.println(data.stringData());
+}
+
+void streamTimeoutCallback(bool timeout) {
+  if (timeout) {
+    Serial.println("Stream timeout, resuming...");
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -32,6 +53,43 @@ void setup() {
 
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
+
+  if (!Firebase.beginStream(firebaseData, "/pins")) {
+    Serial.printf("Stream begin error, %s\n", firebaseData.errorReason().c_str());
+  }
+
+  initializePinData();
+
+  Firebase.setStreamCallback(firebaseData, streamCallback, streamTimeoutCallback);
 }
 
-void loop() {}
+void loop() {
+  if (Firebase.ready()) {
+  }
+}
+
+void initializePinData() {
+  Serial.println("initialized data");
+
+  for (const int &pin : inputPins) {
+    String path = String("/pins/" + String(pin));
+    Firebase.setString(firebaseData, String(path + "/type").c_str(), "input");
+
+    Firebase.setInt(firebaseData, String(path + "/value"), LOW);
+  }
+
+  for (const int &pin : outputPins) {
+    String path = String("/pins/" + String(pin));
+    Serial.println(path);
+    Firebase.setString(firebaseData, String(path + "/type"), "input");
+
+    Firebase.setInt(firebaseData, String(path + "/value"), LOW);
+  }
+
+  // Initialize analog pins
+  Firebase.setString(firebaseData, "/pins/32/type/", "analog");
+  Firebase.setInt(firebaseData, "/pins/32/value/", LOW);
+
+  Firebase.setString(firebaseData, "/pins/25/type/", "digital");
+  Firebase.setInt(firebaseData, "/pins/25/value/", LOW);
+}
