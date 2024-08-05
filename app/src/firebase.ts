4e18ab64efbd97396f1f0ba22644f9a28a2d0745
tomas.lajda <tomas.lajda0@gmail.com>
2024-08-05 21:43:22 +0200

    changed readPin to readPins func

--- app/src/firebase.ts
+++ app/src/firebase.ts
@@ -1,26 +1,24 @@
-import * as dotenv from 'dotenv';
 import { initializeApp } from 'firebase/app';
 import { getDatabase, onValue, ref, set } from 'firebase/database';
 
-dotenv.config();
-
-interface Pin {
+export interface Pin {
   id: number;
   type: string;
   value: number;
 }
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
+
 const db = getDatabase();
 
 export const setPin = ({ id, type, value }: Pin) => {
@@ -32,15 +30,21 @@ export const setPin = ({ id, type, value }: Pin) => {
   });
 };
 
-export const readPin = (id: number) => {
-  const db = getDatabase(app);
-  const reference = ref(db, 'pins/' + id + '/');
+export const readPins = (ids: number[], callback: (pins: Pin[]) => void) => {
+  const pins: Pin[] = [];
+  ids.forEach((id) => {
+    const reference = ref(db, 'pins/' + id + '/');
 
-  onValue(reference, (snapshot) => {
-    const data = snapshot.val();
-    snapshot.forEach((i) => {
-      console.log(i);
+    onValue(reference, (snapshot) => {
+      const data = snapshot.val();
+      const pin = { id, ...data };
+      const index = pins.findIndex((p) => p.id === id);
+      if (index !== -1) {
+        pins[index] = pin;
+      } else {
+        pins.push(pin);
+      }
+      callback([...pins]);
     });
-    console.log('Data changed:', data);
   });
 };
