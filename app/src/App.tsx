012d7430ddfbd3134487866862a6cabea272c944
tomas.lajda <tomas.lajda0@gmail.com>
2024-08-05 21:44:11 +0200

    added simple test  ui to test connection to db

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import reactLogo from './assets/react.svg';
+import { Pin, readPins, setPin } from './firebase'; // Import the functions from your Firebase module
 import viteLogo from '/vite.svg';
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
+  const [pins, setPins] = useState<Pin[]>([]);
+
+  useEffect(() => {
+    const pinIds = [16, 17, 18, 19];
+    readPins(pinIds, (updatedPins: Pin[]) => {
+      setPins(updatedPins);
+    });
+  }, []);
+
   return (
     <>
       <div>
@@ -24,11 +34,23 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <div className='pins'>
+        {pins.map((pin) => (
+          <div key={pin.id}>
+            <p>
+              Pin {pin.id}: {pin.value}
+            </p>
+            <button onClick={() => setPin({ ...pin, value: pin.value + 1 })}>
+              Increment Pin {pin.id}
+            </button>
+          </div>
+        ))}
+      </div>
       <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
     </>
   );
-}
+};
 
 export default App;
