f800b9594fb63e467cc9014ca05244d7a711c620
tomas.lajda <tomas.lajda0@gmail.com>
2024-08-08 18:41:41 +0200

    added select pin to app to test it

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,43 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import reactLogo from './assets/react.svg';
-import { Pin, readPins, setPin } from './firebase'; // Import the functions from your Firebase module
+import SelectPin from './components/select-pin';
+import { Pin, readPins } from './firebase'; // Import the functions from your Firebase module
 import viteLogo from '/vite.svg';
+
+interface Pins {
+  inputPins: Pin[];
+  outputPins: Pin[];
+  analogPins: Pin[];
+}
 
 const App = () => {
   const [count, setCount] = useState(0);
-  const [pins, setPins] = useState<Pin[]>([]);
+  const [pins, setPins] = useState<Pins>({
+    inputPins: [],
+    outputPins: [],
+    analogPins: [],
+  });
 
   useEffect(() => {
     const pinIds = [16, 17, 18, 19];
     readPins(pinIds, (updatedPins: Pin[]) => {
-      setPins(updatedPins);
+      const sortedPins: Pins = {
+        inputPins: [],
+        outputPins: [],
+        analogPins: [],
+      };
+
+      updatedPins.map((pin) => {
+        if (pin.type == 'input') {
+          sortedPins.inputPins.push(pin);
+        } else if (pin.type == 'output') {
+          sortedPins.outputPins.push(pin);
+        } else if (pin.type == 'analog') {
+          sortedPins.analogPins.push(pin);
+        }
+      });
+      setPins(sortedPins);
     });
   }, []);
 
@@ -34,21 +60,12 @@ const App = () => {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <div className='pins'>
-        {pins.map((pin) => (
-          <div key={pin.id}>
-            <p>
-              Pin {pin.id}: {pin.value}
-            </p>
-            <button onClick={() => setPin({ ...pin, value: pin.value + 1 })}>
-              Increment Pin {pin.id}
-            </button>
-          </div>
-        ))}
-      </div>
       <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
+      <SelectPin pins={pins.outputPins} type='output'></SelectPin>
+      <SelectPin pins={pins.inputPins} type='input'></SelectPin>
+      <SelectPin pins={pins.analogPins} type='analog'></SelectPin>
     </>
   );
 };
